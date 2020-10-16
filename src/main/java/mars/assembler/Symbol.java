e74959d16bc64c866ab9c38b888ac15b0dc312ec
Brankale <45881310+Brankale@users.noreply.github.com>
2020-10-16 22:33:45 +0200

    refactoring

--- src/main/java/mars/assembler/Symbol.java
+++ src/main/java/mars/assembler/Symbol.java
@@ -30,70 +30,49 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 /**
  * Represents a MIPS program identifier to be stored in the symbol table.
- *
- * @author Jason Bumgarner, Jason Shrewsbury
- * @version June 2003
- **/
-
+ */
 public class Symbol {
-    private String name;
-    private int address;
-    private boolean data; // boolean true if data symbol false if text symbol.
+
     public static final boolean TEXT_SYMBOL = false;
     public static final boolean DATA_SYMBOL = true;
 
-    /**
-     * Basic constructor, creates a symbol object.
-     *
-     * @param name    The name of the Symbol.
-     * @param address The memroy address that the Symbol refers to.
-     * @param data    The type of Symbol that it is.
-     **/
+    private final String name;
+    private final boolean type;
+    private int address;
 
-    public Symbol(String name, int address, boolean data) {
+    /**
+     * @param name The name of the Symbol.
+     * @param address The memory address that the Symbol refers to.
+     * @param type The type of Symbol.
+     */
+    public Symbol(String name, int address, boolean type) {
         this.name = name;
         this.address = address;
-        this.data = data;
+        this.type = type;
     }
-
-    /**
-     * Returns the address of the the Symbol.
-     *
-     * @return The address of the Symbol.
-     **/
-
-    public int getAddress() {
-        return this.address;
-    }
-
-    /**
-     * Returns the label of the the Symbol.
-     *
-     * @return The label of the Symbol.
-     **/
 
     public String getName() {
         return this.name;
     }
 
     /**
-     * Finds the type of symbol, text or data.
-     *
-     * @return The type of the data.
-     **/
-
+     * @return true if it's a DATA_SYMBOL, false if it's a TEXT_SYMBOL
+     */
     public boolean getType() {
-        return this.data;
+        return this.type;
     }
 
+    public int getAddress() {
+        return this.address;
+    }
 
     /**
-     * Sets (replaces) the address of the the Symbol.
+     * Sets/replaces the address of the the Symbol.
      *
      * @param newAddress The revised address of the Symbol.
-     **/
-
+     */
     public void setAddress(int newAddress) {
         this.address = newAddress;
     }
+
 }
